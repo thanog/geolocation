9d0712264ef5d6beb6888d97f7a143f52ed6e493
Thanasi Giolas <tgiolas@gmail.com>
2019-10-17 15:51:10 -0400

    Adding index and about page, about page is simply just markup/text, index page just has some simple markup and has the UserLocation component contained

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <h1>About Page</h1>
+    <p class="subheading">Test</p>
+
+    <p>Something test123</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About Nuxt",
+      meta: [
+        {
+          name: "description",
+          content: "Default description for this about page test123"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
